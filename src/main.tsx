4294fccc685250e5ab6ab497169818a212ded3fa
geo bars <georgiybarsegian@yandex.com>
2026-01-23 20:05:26 +0300

    -оформить красиво и с пояснениями main

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+
+const domContainer = document.getElementById('root')!;
+
+
+/**
+ * Создаёт объект FiberRoot — специальную структуру данных React, которая:
+ * - Управляет рендерингом React-компонентов внутри этого контейнера
+ * - Поддерживает Concurrent Features (приоритетные обновления, прерываемый рендеринг)
+ * - Содержит ссылки на Virtual DOM (Fiber tree) и другие внутренности React
+ */
+const reactRoot = createRoot(domContainer);
+
+
+/** Запускает процесс рендеринга компонента App внутрь DOM-контейнера. **/
+reactRoot.render(
   <StrictMode>
     <App />
   </StrictMode>,
