14b39fd640f6a29875074acd8c562c67dcedc13a
geo bars <georgiybarsegian@yandex.com>
2026-01-30 19:18:34 +0300

    -скоратить импорт

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { App } from "./widgets/app";
+import { App } from "@/widgets";
 
 import './styles/global.css';
 import './styles/variables.css';
