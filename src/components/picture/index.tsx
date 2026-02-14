21cf69cc8a074b8c9545652ef468de569517b0d9
geo bars <georgiybarsegian@yandex.com>
2026-02-14 23:33:21 +0300

    -задать уточняющие пропсы для компоненты картины

--- src/components/picture/index.tsx
+++ src/components/picture/index.tsx
@@ -1,24 +1,24 @@
 import {type FC} from 'react';
+import {classNames} from "@/helpers";
 
 type PictureProps = {
-  height: number;
   width: number;
-  caption: string;
+  caption: { title: string, position: 'horizontal' | 'vertical' };
   src: string;
   size: {w: number, h: number};
 };
 
 export const Picture: FC<PictureProps> = ({
-  height,
   width,
   caption,
   size,
   src
 }) => {
+  const cls = classNames('picture-container', {horizontal: caption.position === 'horizontal'})
   return (
-    <div className={'picture-container'} >
-      <img src={src} className={'picture__img'} style={{height, width}} />
-      <span className={'picture__caption'}>{caption} {size.w} X {size.h}</span>
+    <div className={cls} >
+      <img src={src} className={'picture__img'} style={{width}} />
+      <span className={'picture__caption'}>{caption.title} {size.w} X {size.h}</span>
     </div>
   );
 };
--- src/components/sections/paintings/index.tsx
+++ src/components/sections/paintings/index.tsx
@@ -1,6 +1,11 @@
 import {type FC} from 'react';
 
+import {Picture} from "@/components";
+
+import COWS from '@/assets/pictures/Cow.jpg';
+
 import './index.scss';
+
 
 
 type PaintingsProps = {}
@@ -8,6 +13,10 @@ type PaintingsProps = {}
 export const Paintings: FC<PaintingsProps> = () => {
 
   return (
-    <div className={'paintings-container'}>Paintings</div>
+    <div className={'paintings-container'}>
+      Paintings
+      <Picture width={800} caption={{title: 'Кофе и сигареты', position: "vertical"}} src={COWS} size={{w: 350, h: 200 }}/>
+
+    </div>
   );
 };
