76f8ff3da4da9be4dfc2b2311eabc148c19a0605
geo bars <georgiybarsegian@yandex.com>
2026-03-26 21:13:05 +0300

    -добавить паралакс эффект на секцию ArtistStatement

--- src/components/sections/artistStatement/index.tsx
+++ src/components/sections/artistStatement/index.tsx
@@ -1,22 +1,45 @@
-import {type FC} from 'react';
+import { type FC, useEffect, useRef, useState } from 'react';
 
-import {useLocalization} from "@/providers";
-import {Typography} from "@/components";
+import { useLocalization } from "@/providers";
+import { Typography } from "@/components";
 
 import './index.scss';
-
-
 
 type ArtistStatementProps = {};
 
 export const ArtistStatement: FC<ArtistStatementProps> = () => {
-  const {translate} = useLocalization();
+  const { translate } = useLocalization();
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [isInView, setIsInView] = useState(false);
 
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        // Убираем анимацию при уходе из области видимости
+        if (!entry.isIntersecting) {
+          setIsInView(false);
+        } else {
+          setIsInView(true);
+        }
+      },
+      {
+        threshold: 0.3, // Срабатывает при 30% видимости блока
+      }
+    );
 
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+
+    return () => observer.disconnect();
+  }, []);
 
   return (
-    <div className={'as-container'}>
-      <div className={'as__cover'}/>
+    <div
+      ref={containerRef}
+      className={`as-container ${isInView ? 'in-view' : ''}`}
+    >
+      <div className={'as__cover'} />
       <div className={'as__content'}>
         <div className={'as__text'}>
           <Typography tag={'span'} className="as__title" family={'pt'} weight={'bold'}>
